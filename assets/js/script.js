69d65ccbc7d0cab0b9a56af476bc8bb0df0634c5
DabneyRicardo <82227488+DabneyRicardo@users.noreply.github.com>
2022-01-27 15:23:14 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,72 @@
-// TODO: Declare any global variables we need
+//Coin flip variables
+let coin = 1;
+let num_heads = 0;
+let num_tails = 0;
+let num_total = 0;
+
+let percent_heads = 0;
+let percent_tails = 0;
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
 
-    // TODO: Add event listener and handler for flip and clear buttons
-
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
+//Update the chart variables
+setInterval(function(){
+    //updates chart with variables in real time
+    document.getElementById("heads").innerHTML              = num_heads;
+    document.getElementById("heads-percent").innerHTML      = percent_heads + '%';
+    document.getElementById("tails").innerHTML              = num_tails;
+    document.getElementById("tails-percent").innerHTML      = percent_tails + '%';
 
 
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
+    // COIN ANIMATION STATE
+if (num_total > 0)    {
+    if (coin == 1) {
+        document.getElementById("coin-heads").src= "assets/images/penny-heads.jpg"
+        document.getElementById('message').textContent = 'You Flipped Heads!'
+    };
 
+    if (coin == 2) {
+        document.getElementById("coin-heads").src= "assets/images/penny-tails.jpg"
+        document.getElementById('message').textContent = 'You Flipped Tails!'
+    };
+};
+
+},1);
+
+
+
+// Flip the coin  element was flip
+document.getElementById('flip').addEventListener('click', function () {
+    console.log('coin was flipped successfully!')
+    coin = Math.ceil(Math.random() * 2);   {
+        if (coin == 1) {num_heads ++};
+        if (coin == 2) {num_tails ++};
+          //calculates the total number of flips
+          num_total = (num_heads + num_tails)
+
+         {
+        console.log('coin: '+coin)
+        console.log('num_heads: '+num_heads)
+        console.log('num_total: '+num_total)
+        console.log('percent_heads: '+percent_heads)
+
+        // calculates the total % of heads and tails if the total num of flips > 0 
+        if (num_total > 0) {percent_heads = Math.round((num_heads / num_total) * 100)}
+        if (num_total > 0) {percent_tails = Math.round((num_tails / num_total) * 100)}
+
+            //calculates the total number of flips
+        num_total = (num_heads + num_tails)
+        }
+    }
+})
+
+
+// Flip the coin  element was flip
+document.getElementById('clear').addEventListener('click', function () {
+    console.log('Cleared Successfully!')
+    num_heads = 0
+    num_tails = 0 
+    num_total = 0;
+    percent_heads = 0;
+    percent_tails = 0;
 })
